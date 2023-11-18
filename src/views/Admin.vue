c87dd4ea54080421486137ba5b26fa82df098c4e
paulvinzent <manalopv@gmail.com>
2023-11-19 02:20:40 +0800

    init_admin

---
+++ src/views/Admin.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="admin-panel">
+      <header>
+        <h1>Poultry Management System Admin Panel</h1>
+      </header>
+  
+      <main>
+        <nav>
+          <ul>
+            <li><router-link to="/users">Users</router-link></li>
+            <li><router-link to="/branches">Branches</router-link></li>
+            <li><router-link to="/products">Products</router-link></li>
+            <li><router-link to="/notifications">Notifications</router-link></li>
+          </ul>
+        </nav>
+  
+        <router-view></router-view>
+      </main>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    mounted() {
+      // Check if user is logged in and has admin privileges
+      if (!this.$store.state.user.isLoggedIn || !this.$store.state.user.isAdmin) {
+        this.$router.push('/login');
+      }
+    },
+  };
+  </script>
+  
